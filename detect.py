b759a702217b4887659674da159013d6bd350575
Akash Mondal <mondalakash63@gmail.com>
2024-02-23 02:41:33 +0530

    Add seperate colored boxes around known and unknown faces

--- detect.py
+++ detect.py
@@ -71,13 +71,21 @@ while True:
         bottom *= 4
         left *= 4
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        # Draw a red box around the unknown face and green box around the known face
+        if name == "Unknown":
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Draw a label with a name below the face
-        cv2.rectangle(
-            frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED
-        )
+            # Draw a label with a name below the face
+            cv2.rectangle(
+                frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED
+            )
+        else:
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+
+            cv2.rectangle(
+                frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED
+            )
+
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
