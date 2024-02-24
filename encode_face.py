e74991954fd347a2c0687006a5ffc31b48195ca8
Akash Mondal <mondalakash63@gmail.com>
2024-02-25 02:26:57 +0530

    Face.load_faces now returns face names also

--- encode_face.py
+++ encode_face.py
@@ -13,8 +13,8 @@ def connect():
 
 
 class Face:
+    # add the face encoding bytes to the database for the specific person
     def add_face(self, img_file, name):
-        """Todo: add the face encoding bytes to the database for the specific person"""
         img = load_image_file(img_file)
         faceloc = face_locations(img)
         face_encode = face_encodings(img, faceloc, 1)[0]
@@ -31,18 +31,19 @@ class Face:
         except sqlite3.IntegrityError:
             return "Person already exists!"
 
+    # read all the face_encodings and names from the database and return and list containg all the known face_encodings and name
     def load_faces(self):
         """Todo: read all the face_encodings from the database and return and list
         Containg all the known face_encodings"""
         con = connect()
         cur = con.cursor()
-        cur.execute("SELECT face_encoding FROM persons")
+        cur.execute("SELECT name,face_encoding FROM persons")
         encodings = cur.fetchall()
-        known_face_encodings = [encoding[0] for encoding in encodings]
-
+        known_face_encodings = [encoding[1] for encoding in encodings]
+        know_face_names = [name[0] for name in encodings]
         known_face_encodings = [
             np.frombuffer(encoding, dtype="float64")
             for encoding in known_face_encodings
         ]
         con.close()
-        return known_face_encodings
+        return known_face_encodings, know_face_names
