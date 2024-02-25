c151d91b3e34c00c75e14a3a1c9a83aa16fccbca
Akash Mondal <mondalakash63@gmail.com>
2024-02-25 23:05:17 +0530

    Add attendance table to the database schema

--- schema.sql
+++ schema.sql
@@ -1,6 +1,13 @@
 CREATE TABLE persons(
-id INTEGER PRIMARY KEY,
-name TEXT NOT NULL,
-emp_id TEXT NOT NULL,
-face_encoding BLOB UNIQUE NOT NULL
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    emp_id TEXT NOT NULL,
+    face_encoding BLOB UNIQUE NOT NULL
 );
+
+CREATE TABLE attendance(
+    id INTEGER PRIMARY KEY,
+    date DATE NOT NULL,
+    person_id INTEGER NOT NULL,
+    FOREIGN KEY (person_id) REFERENCES persons(id)
+)
