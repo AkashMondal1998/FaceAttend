79e105611c25ebd38eeb75b13e08aa153fc1c048
Akash Mondal <mondalakash63@gmail.com>
2024-02-28 11:32:29 +0530

    Update tolerance and num_jitters

--- detect.py
+++ detect.py
@@ -1,5 +1,4 @@
 import face_recognition
-import os
 import cv2
 import numpy as np
 from face import Face
@@ -33,7 +32,7 @@ while True:
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(
-            rgb_small_frame, face_locations, 2
+            rgb_small_frame, face_locations, 1
         )
 
         face_names = []
@@ -41,7 +40,7 @@ while True:
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(
-                known_face_encodings, face_encoding, tolerance=0.4
+                known_face_encodings, face_encoding, tolerance=0.5
             )
             name = "Stranger"
 
