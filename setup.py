37d500ecd2ab37dec9b3bf040454b1f281e2d323
Akash Mondal <mondalakash63@gmail.com>
2024-03-04 02:53:09 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup
 
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="faceattend",
-    version="0.0.1",
-    description="A attendance system using face recognition",
+    version="0.0.2",
+    description="An attendance system using face recognition",
     url="https://github.com/AkashMondal1998/FaceAttend",
     author="Akash Mondal",
     author_email="mondalakash63@gmail.com",
     license="MIT license",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=["faceattend"],
     install_requires=["face-recognition", "gTTS", "pygame", "opencv-python"],
     classifiers=[
