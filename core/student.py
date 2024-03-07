f28795f6f8cb3d4bc33a4ca6065b344d7cd68d58
Akash Mondal <mondalakash63@gmail.com>
2024-03-08 00:00:08 +0530

    clean up

--- core/student.py
+++ core/student.py
@@ -1,20 +1,25 @@
 from .utils import generate_std_id, send_email
 import numpy as np
+from face_recognition import face_locations, face_encodings
 
 
 class Student:
     # Add the name ,std_id email,face encoding bytes to the database for the specific student
     @staticmethod
     def add(name, email, img_file):
+
+        # convert the base64 encoded string to numpy array
         img = np.frombuffer(img_file, dtype="uint8")
         faceloc = face_locations(img)
-        face_encode = face_encodings(img, faceloc, 1)[0]
-        emp_id = generate_emp_id()
+        face_encoding = face_encodings(img, faceloc, 1)[0]
+
+        # generate the student_id
+        std_id = generate_std_id()
         cur.execute(
             "INSERT INTO persons (name,email,emp_id,face_encoding) VALUES(?,?,?,?)",
-            (name, email.normalized, emp_id, face_encode.tobytes()),
+            (name, email, std_id, face_encoding.tobytes()),
         )
         con.commit()
         con.close()
-        send_email(name, emp_id, email.normalized)
-        return f"Face added for {name} with employee id {emp_id}"
+        send_email(name, std_id, email.normalized)
+        return f"Face added for {name} with employee id {std_id}"
