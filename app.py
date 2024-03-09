fdb20f9eb03ca231c93d5420ba0a508ae1f1dca3
Akash Mondal <mondalakash63@gmail.com>
2024-03-09 15:17:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from core.extensions import mysql
 from apis import api
+import os
 
 UPLOAD_FOLDER = "photos"
 
@@ -8,8 +9,8 @@ app = Flask(__name__, static_folder=UPLOAD_FOLDER)
 
 
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-app.config["MYSQL_USER"] = "akash"
-app.config["MYSQL_PASSWORD"] = "akash"
+app.config["MYSQL_USER"] = os.environ["db_user"]
+app.config["MYSQL_PASSWORD"] = os.environ["db_pass"]
 app.config["MYSQL_DB"] = "project"
 
 api.init_app(app)
@@ -17,4 +18,5 @@ mysql.init_app(app)
 
 
 if __name__ == "__main__":
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
     app.run(debug=True, port=8080)
