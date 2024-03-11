344a3aa7ad7c5827318f245232cab2eec885fe83
Akash Mondal <mondalakash63@gmail.com>
2024-03-11 14:29:40 +0530

    New api response model Cleaned up the code

--- apis/student.py
+++ apis/student.py
@@ -11,8 +11,19 @@ api = Namespace("students", description="Student related operations")
 
 UPLOAD_FOLDER = "photos"
 
-student_model = api.model(
+# student list api response model
+student_list_model = api.model(
     "Student_list",
+    {
+        "name": fields.String(description="Name of the student"),
+        "email": fields.String(description="Email of the student"),
+        "std_id": fields.String(description="Student id of the student"),
+    },
+)
+
+# student api response model
+student_model = api.model(
+    "Student",
     {
         "name": fields.String(description="Name of the student"),
         "email": fields.String(description="Email of the student"),
@@ -24,22 +35,18 @@ student_model = api.model(
 
 @api.route("/add")
 class AddStudent(Resource):
-    """
-    Add student info to the database and also accept an image file of the student
-    save the file to the file system
-    Expect form data containg name,email,and an image file from the client
-    """
 
     def post(self):
+        """Add a student"""
         image = request.files.get("image")
         if not image:
-            abort(400, "Image is requried!")
+            abort(400, "Image is requried")
         name = request.form.get("name")
         if not name:
-            abort(400, "Name cannnot be blank!")
+            abort(400, "Name is required")
         email = request.form.get("email")
         if not email:
-            abort(400, "Email cannot be blank")
+            abort(400, "Email is required")
 
         # check if email is valid
         email, ret = check_email(email)
@@ -63,10 +70,11 @@ class AddStudent(Resource):
 
 @api.route("/get")
 class GetAllStudents(Resource):
-    """Return the details of all the students"""
 
-    @api.marshal_list_with(student_model)
+    @api.marshal_list_with(student_list_model)
     def get(self):
+        """Get  details of all the students"""
+
         if not Student.student_list():
             return " ", 204
         return Student.student_list()
@@ -74,10 +82,11 @@ class GetAllStudents(Resource):
 
 @api.route("/get/<std_id>")
 class GetStudent(Resource):
-    """Returns the detail of a single student given their student id"""
 
     @api.marshal_with(student_model)
     def get(self, std_id):
+        """Get details of a single student given their student id"""
+
         if not Student.student(std_id):
             abort(404, f"Student with id {std_id} not found!")
         return Student.student(std_id)
@@ -85,9 +94,10 @@ class GetStudent(Resource):
 
 @api.route("/delete/<std_id>")
 class DeleteStudent(Resource):
-    """Delete a student from the database"""
 
     def delete(self, std_id):
+        """Delete a student"""
+
         if not Student.delete(std_id):
             abort(404, f"Student with id {std_id} not found!")
         return {"message": "Student successfully removed!"}, 200
@@ -95,10 +105,11 @@ class DeleteStudent(Resource):
 
 @api.route("/csv")
 class StudentCsv(Resource):
-    """Generate a csv file containing all the student details"""
 
     @before_request
     def get(self):
+        """Generate a csv file containing all the student details"""
+
         @after_this_request
         def delete_file(response):
             os.remove("StudentList.csv")
