3657d96671cbbd8c03076a4dbfb8cbce72b7f443
Akash Mondal <mondalakash63@gmail.com>
2024-03-11 14:28:36 +0530

    Fixed typo

--- core/helpers.py
+++ core/helpers.py
@@ -1,13 +1,15 @@
 from .student import Student
+from functools import wraps
 
 
 def before_request(f):
     """Decorator to check if students are present"""
 
-    def wrapper(*agrs, **kwargs):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
         # check if students are present
         if not Student.student_list():
             return " ", 204
-        return f(*agrs, **kwargs)
+        return f(*args, **kwargs)
 
     return wrapper
