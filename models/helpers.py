d8135efa73044696f4e22049d2fce963ce744b6c
Akash Mondal <mondalakash63@gmail.com>
2024-03-13 17:37:59 +0530

    Update helpers.py

--- models/helpers.py
+++ models/helpers.py
@@ -9,22 +9,29 @@ def before_request(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
 
+        # check if the user is logged in or not
         if "username" not in session:
-            abort(401, "Please Login to accesss")
+            abort(401, "Access Denied. Please log in to access this resource")
+
         # check if students are present
         if not Student.student_list():
             return " ", 204
+
         return f(*args, **kwargs)
 
     return wrapper
 
 
 def login_required(f):
+    """Decorator to check if the user if logged in"""
 
     @wraps(f)
     def wrapper(*args, **kwargs):
+
+        # check if the user is logged in or not
         if "username" not in session:
-            abort(401, "Please Login to access")
+            abort(401, "Access Denied. Please log in to access this resource")
+
         return f(*args, **kwargs)
 
     return wrapper
